1ce58eff4f0a9f444f094b4cae9847acd8687b1f
paravozz <prvz-work@outlook.com>
2024-10-12 02:04:50 +0100

    [fix] change transpile target

--- api/transpile.ts
+++ api/transpile.ts
@@ -1,11 +1,10 @@
-import { transpileModule, ModuleKind } from "typescript";
+import { ModuleKind, ScriptTarget, transpileModule } from "typescript";
 
 
 export async function POST(request: Request) {
-  console.log("GOT REQUEST");
   const source = await request.text();
 
-  let result = transpileModule(source, { compilerOptions: { module: ModuleKind.ESNext }});
+  let result = transpileModule(source, { compilerOptions: { module: ModuleKind.ESNext, target: ScriptTarget.ESNext }});
 
   return new Response(result.outputText, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
